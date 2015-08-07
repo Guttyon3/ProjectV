1b8352b3a96b84ceebb8bfc8bc244a57c8d57d25
Guttyon3 <y.yamaguchi0329@gmail.com>
2015-08-08 07:53:58 +0900

    ゲームデータセレクト画面の作成とゲームシーンへ遷移する
    ・仮タイトル配置
    ・”START”, “CONTINUE” ボタンの配置
    ・”START”ボタンを押すことで、ゲームシーンへ遷移する
    ・ゲームシーンの作成（空白）

---
+++ ProjectV/Assets/scripts/Setting/GameSelect/UI/startButtonScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class startButtonScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// ボタンをクリックするとゲームシーンへ移動する
+	public void OnClick() {
+
+		// ゲームシーンへ移動
+		Debug.Log("Start Game!");
+		Application.LoadLevel ("GameScene");
+	}
+}
---
+++ ProjectV/Assets/scripts/Setting/SceanChange/sceanChange.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * 指定したシーンに移動する
+ * マリオでいうゴールフラッグ的なものににアタッチすることを基本とする。
+ * 2015/07/28 Tue - Guttyon
+*/
+
+using UnityEngine;
+using System.Collections;
+
+public class sceanChange : MonoBehaviour {
+
+	// public staticはシーンが移動しても参照可能
+	public static string sceanName = "StartMenu";
+
+	// getter
+	public static string getSceanName() {
+		return sceanName;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+		/* 
+		 * DontDestroyOnload("GameObjectName");
+		 * シーンを切り替えてもアタッチしたGameObjectを削除しないようにする
+		 * gameControllやシーン遷移プログラムなど、ゲーム全体に影響するプログラムに使用
+		*/
+		DontDestroyOnLoad(this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnCollisionEnter2D(Collision2D coll)
+	{
+		// 基本、プレイヤーに接触すると次のシーンに移動
+		if (coll.gameObject.name == "Player") {
+
+			// 現在のシーン名に合わせて、次のシーンに移動
+			if (Application.loadedLevelName == "sceanName") {
+				Application.LoadLevel ("nextSceanName");
+			}
+		}
+	}
+}
