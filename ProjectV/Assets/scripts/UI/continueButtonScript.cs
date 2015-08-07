debffa591eaa084e59a38209f0091ee5e9b260a4
Guttyon3 <y.yamaguchi0329@gmail.com>
2015-08-08 08:27:25 +0900

    ゲームデータ選択画面作成
    ・ゲームメニュー画面と選択画面を分離
    ・ゲームデータ選択画面の作成

---
+++ ProjectV/Assets/scripts/UI/continueButtonScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class continueButtonScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	
+	// ボタンをクリックするとセーブデータセレクトシーンへ移動する
+	public void OnClick() {
+		
+		// ゲームシーンへ移動
+		Debug.Log("Data Select");
+		Application.LoadLevel ("GameSelect");
+	}
+}
